c53fd4b9083a2d04387ef5b15b997e279ee67efd
joseph <josephtkao@gmail.com>
2021-09-11 19:01:46 -0500

    make tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+class TaskList{
+  constructor(){
+      this.tasks=[];
+  }
+
+  createNewTask(desc){
+      const newTask = new Task(desc);
+      this.tasks.push(newTask);
+  }
+
+  renderTasks(){
+      return this.tasks.map((task) => task.render()).join('');
+  }
+}
+
+class Task{
+    constructor(desc){
+        this.desc = desc;
+    }
+
+    render(){
+      return `<li> ${this.desc}</li>`
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const myList = new TaskList();
+  const input = document.getElementById('create-task-form');
+  const taskDesc = document.getElementById('new-task-description');
+  const tasks = document.getElementById('tasks')
+  
+  const renderPage=() =>(tasks.innerHTML = myList.renderTasks());
+  
+
+  function logSubmit(e){
+    e.preventDefault();
+    myList.createNewTask(taskDesc.value);
+    e.target.reset();
+    renderPage()
+  }
+
+
+  input.addEventListener('submit', logSubmit);
 });
